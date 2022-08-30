c0d70895f377f52cb6fc510e1ce5b29e0664c9e0
shakhbozpro <shakhbozmamatkulov111@gmail.com>
2022-08-30 19:23:56 +0500

    Finished home page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var elNavOpenBtn = document.querySelector(".js-nav-open");
+var elSiteHeader = document.querySelector(".site-header");
+
+elNavOpenBtn.addEventListener("click" , function() {
+   elSiteHeader.classList.toggle("site-nav-open");
+
+   elNavOpenBtn.classList.toggle("js-btn-open");
+})
